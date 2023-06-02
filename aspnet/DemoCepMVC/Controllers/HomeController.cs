057bd9f39d5031f3585561c7c1ac347cd9b8c4c6
Júlio Machado <julioapm@gmail.com>
2023-06-02 16:29:39 -0300

    DemoCepMVC

---
+++ aspnet/DemoCepMVC/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DemoCepMVC.Models;
+using DemoCepMVC.Services;
+
+namespace DemoCepMVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly ICepRepository _repository;
+
+    public HomeController(ILogger<HomeController> logger, ICepRepository repository)
+    {
+        _logger = logger;
+        _repository = repository;
+    }
+
+    public IActionResult Index()
+    {
+        var ceps = _repository.ConsultaTodos();
+        var cepsViewModel = ceps.Select(c => CepViewModel.FromModel(c));
+        return View(cepsViewModel);
+    }
+
+    //GET .../Home/Search?id=
+    public IActionResult Search(string id)
+    {
+        ViewData["Id"] = id;
+        CepModel? cep = null;
+        if (!String.IsNullOrWhiteSpace(id))
+        {
+            cep = _repository.ConsultaPorCodigo(id);
+        }
+        return View(cep is not null ? CepViewModel.FromModel(cep) : cep);
+    }
+
+    //GET .../Home/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    //POST .../Home/Create
+    [HttpPost]
+    public IActionResult Create(CepViewModel novoCep)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(novoCep);
+        }
+        _repository.Cadastrar(CepModel.FromViewModel(novoCep));
+        return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ aspnet/DemoCepMVC/Models/Cep.cs
@@ -0,0 +1,48 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DemoCepMVC.Models;
+
+public class CepModel
+{
+    public string Cep {get;set;} = null!;
+    public string Estado {get;set;} = null!;
+    public string Cidade {get;set;} = null!;
+    public string Bairro {get;set;} = null!;
+    public string Logradouro {get;set;} = null!;
+
+    public static CepModel FromViewModel(CepViewModel model)
+    {
+        return new CepModel {
+            Cep = model.Cep,
+            Estado = model.Estado,
+            Cidade = model.Cidade,
+            Bairro = model.Bairro,
+            Logradouro = model.Logradouro
+        };
+    }
+}
+
+public class CepViewModel
+{
+    [RegularExpression(@"^\d{8}$", ErrorMessage = "Cep deve conter exatamente 8 dígitos")]
+    public string Cep {get;set;} = null!;
+    [RegularExpression(@"^[A-Z]{2}$", ErrorMessage = "Estado dever ser uma sigla com duas letras maiúsculas")]
+    public string Estado {get;set;} = null!;
+    [StringLength(50, ErrorMessage = "Cidade deve ter no máximo 50 caracteres")]
+    public string Cidade {get;set;} = null!;
+    [StringLength(30, ErrorMessage = "Bairro deve ter no máximo 30 caracteres")]
+    public string Bairro {get;set;} = null!;
+    [StringLength(70, ErrorMessage = "Logradouro deve ter no máximo 70 caracteres")]
+    public string Logradouro {get;set;} = null!;
+
+    public static CepViewModel FromModel(CepModel model)
+    {
+        return new CepViewModel {
+            Cep = model.Cep,
+            Estado = model.Estado,
+            Cidade = model.Cidade,
+            Bairro = model.Bairro,
+            Logradouro = model.Logradouro
+        };
+    }
+}
