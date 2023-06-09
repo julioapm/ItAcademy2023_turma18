ceb31287ebc1b045b99747503db8fc7327155af2
Júlio Machado <julioapm@gmail.com>
2023-06-09 14:49:49 -0300

    DemoEFWebAPI models

--- entityframework/DemoEFWebApi/Models/Pedido.cs
+++ entityframework/DemoEFWebApi/Models/Pedido.cs
@@ -7,5 +7,7 @@ public class Pedido
     //relacionamento N-1 com Cliente
     public Cliente Cliente {get;set;} = null!;
     public int ClienteId {get;set;}
-    
+    //relacionamento N-N com Produto
+    public ICollection<Produto> Produtos {get;set;} = null!;
+    public List<Item> Itens {get;set;} = null!;
 }
