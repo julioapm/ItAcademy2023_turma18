97150c1a7138cb3a67971da9f384e88fa2c10963
Júlio Machado <julioapm@gmail.com>
2023-06-22 17:11:57 -0300

    demo typescript4 promises

--- typescript/demo_typescript4/src/index.ts
+++ typescript/demo_typescript4/src/index.ts
@@ -1,6 +1,7 @@
 import { area, potenciaDois as pot2 } from './circulo_funcoes';
 import Circulo from './circulo_objeto';
 import * as fs from 'node:fs';
+import * as fsasync from 'node:fs/promises';
 
 console.log(area(5));
 let c = new Circulo(5);
@@ -9,3 +10,32 @@ console.log(pot2(3));
 
 const json = JSON.stringify(c);
 fs.writeFileSync('dados.json', json);
+/*
+fs.readFile('dados.json', 'utf-8', (err, data) => {
+    if (err) {
+        console.error('Falha de leitura do arquivo');
+        console.error(err.message);
+    } else {
+        console.log(data);
+    }
+});
+*/
+/*
+fsasync.readFile('dados.json', 'utf-8')
+       .then(data => console.log(data))
+       .catch(err => {
+            console.error('Falha de leitura do arquivo');
+            console.log(err);
+       });
+*/
+async function main() {
+    try {
+        const data = await fsasync.readFile('dados.json', 'utf-8');
+        console.log(data);
+    } catch (err) {
+        console.error('Falha de leitura do arquivo');
+        console.log(err);
+    }   
+}
+
+main();
