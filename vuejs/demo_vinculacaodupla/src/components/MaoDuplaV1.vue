8fd147ceff0113e82cf64851b5144dd03badec6a
Júlio Machado <julioapm@gmail.com>
2023-06-28 17:14:19 -0300

    demo vinculacao dupla

---
+++ vuejs/demo_vinculacaodupla/src/components/MaoDuplaV1.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const valorTexto = ref('');
+function handleInput(event: Event) {
+    valorTexto.value = (event.target as HTMLInputElement).value;
+}
+</script>
+
+<template>
+    <div>
+        <input type="text" placeholder="Digite seu nome"
+            :value="valorTexto"
+            @input="handleInput"
+        />
+        <span>{{ valorTexto }}</span>
+    </div>
+</template>
